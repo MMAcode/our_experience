099e7258ce21d423e04931893d27468b3f2b02a2
MIROslavMakarov <miroslav.makarov@gmail.com>
2022-12-02 09:48:19 +0000

    alow port to be set from terminal

--- config/runtime.exs
+++ config/runtime.exs
@@ -141,4 +141,24 @@ config :our_experience, OurExperienceWeb.Endpoint,
     transport_options: [socket_opts: [:inet6]]
   ]
 
+host = System.get_env("PHX_HOST") || "ourexperience.info"
+port = String.to_integer(System.get_env("PORT") || "4000")
+
+config :our_experience, OurExperienceWeb.Endpoint,
+  server: true,
+  secret_key_base: "f8MVhmeSUq4qW1POObdVHJCbU8xqld5747SRY6LYXRgu1ZO9JRUurtyQL5LLYz54",
+
+  url: [host: host, port: 443, scheme: "https"],
+  http: [
+    # Enable IPv6 and bind on all interfaces.
+    # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+    # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+    # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+    ip: {0, 0, 0, 0, 0, 0, 0, 0},
+    port: port,
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  # secret_key_base: secret_key_base
+
+
 end
