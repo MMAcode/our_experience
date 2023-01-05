675e9436857a92572a28fa15db24b626b3610b7c
MIROslavMakarov <miroslav.makarov@gmail.com>
2023-01-05 18:50:40 +0000

    logout fixed

--- lib/our_experience_web/controllers/auth/auth_controller.ex
+++ lib/our_experience_web/controllers/auth/auth_controller.ex
@@ -4,23 +4,43 @@ defmodule OurExperienceWeb.Auth.AuthController do
   plug Ueberauth
 
   def logout(conn, _params) do
-    dbg "miro1234"
+    # dbg(["mm logout f1.:", Application.loaded_applications()], printable_limit: :infinity, limit: :infinity) #get names of all apps
+    # dbg(["mm logout f2.:", Application.get_all_env(:ueberauth)], printable_limit: :infinity, limit: :infinity) #get all env vars in specific app
+    # dbg(["mm logout f3.:", Application.get_env(:ueberauth, Ueberauth.Strategy.Auth0.OAuth)], printable_limit: :infinity, limit: :infinity)
+    auth0_secrets = Application.get_env(:ueberauth, Ueberauth.Strategy.Auth0.OAuth)
+    query = %{
+      "returnTo" => "http://localhost:4000",
+      "client_id" => auth0_secrets[:client_id]
+    }
+    encoded_query = URI.encode_query(query)
+    url = "https://#{auth0_secrets[:domain]}/v2/logout?#{encoded_query}" |> dbg
+
+
     conn
+    # |> Session.logout()
     |> configure_session(drop: true)
     |> clear_session()
     |> put_flash(:info, "You have been logged out!")
-    |> redirect(to: "/")
+    |> redirect(external: url)
+
+    # conn
+    # |> configure_session(drop: true)
+    # |> clear_session()
+    # |> put_flash(:info, "You have been logged out!")
+    # |> redirect(to: "/")
   end
 
   def callback(%{assigns: %{ueberauth_failure: _fails}} = conn, _params) do
-    dbg "miro123"
+    dbg("miro123")
+
     conn
     |> put_flash(:error, "Failed to authenticate.")
     |> redirect(to: "/")
   end
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
-    dbg ["miro12",conn]
+    dbg(["miro12", conn])
+
     case UserFromAuth.find_or_create(auth) do
       {:ok, user} ->
         conn
@@ -28,6 +48,7 @@ defmodule OurExperienceWeb.Auth.AuthController do
         |> put_session(:current_user, user)
         # |> configure_session(renew: true)
         |> redirect(to: "/")
+
       {:error, reason} ->
         conn
         |> put_flash(:error, reason)
