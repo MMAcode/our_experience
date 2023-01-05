611ef2cb921e8bb28a66c494e9884bcf9822e9d0
MIROslavMakarov <miroslav.makarov@gmail.com>
2023-01-05 20:34:04 +0000

    what data are received with login from auth0

--- lib/our_experience_web/controllers/auth/auth_controller.ex
+++ lib/our_experience_web/controllers/auth/auth_controller.ex
@@ -12,13 +12,14 @@ defmodule OurExperienceWeb.Auth.AuthController do
 
     # endpoint = Application.get_env(:our_experience, OurExperienceWeb.Endpoint)[:url][:host] |> dbg
     return_url = Application.get_env(:our_experience, Miro)[:logout_url]
+
     query = %{
       "returnTo" => return_url,
       "client_id" => auth0_secrets[:client_id]
     }
+
     encoded_query = URI.encode_query(query)
     url = "https://#{auth0_secrets[:domain]}/v2/logout?#{encoded_query}" |> dbg
-
 
     conn
     # |> Session.logout()
@@ -44,6 +45,41 @@ defmodule OurExperienceWeb.Auth.AuthController do
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
     dbg(["miro12", conn])
+
+    # ueberauth_auth: %Ueberauth.Auth{
+    #     uid: "google-oauth2|110722583238847144647",
+    #     provider: :auth0,
+    #     strategy: Ueberauth.Strategy.Auth0,
+    #     info: %Ueberauth.Auth.Info{
+    #       name: "Miroslav Makarov",
+    #       first_name: "Miroslav",
+    #       last_name: "Makarov",
+    #       nickname: "miroslav.makarov",
+    #       email: "miroslav.makarov@gmail.com",
+    #       location: nil,
+    #       description: nil,
+    #       image: "https://lh3.googleusercontent.com/a/AEdFTp735vTUemsSP1MyJiSjp6n7hb3dqCYY8RcQYzMBUkg=s96-c",
+    #       phone: nil,
+    #       birthday: nil,
+    #       urls: %{profile: nil, website: nil}
+
+    # ueberauth_auth: %Ueberauth.Auth{
+    #     uid: "auth0|63b60ba0cb240538d9e86fa1",
+    #     provider: :auth0,
+    #     strategy: Ueberauth.Strategy.Auth0,
+    #     info: %Ueberauth.Auth.Info{
+    #       name: "miroslav.makarov@gmail.com",
+    #       first_name: nil,
+    #       last_name: nil,
+    #       nickname: "miroslav.makarov",
+    #       email: "miroslav.makarov@gmail.com",
+    #       location: nil,
+    #       description: nil,
+    #       image: "https://s.gravatar.com/avatar/f0a49c301321756b2731cb040799c8ec?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fmi.png",
+    #       phone: nil,
+    #       birthday: nil,
+    #       urls: %{profile: nil, website: nil}
+    #     },
 
     case UserFromAuth.find_or_create(auth) do
       {:ok, user} ->
