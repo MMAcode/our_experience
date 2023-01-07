6a8881c6ec296289441a7d74a7b4232183b626f8
MIROslavMakarov <miroslav.makarov@gmail.com>
2023-01-07 01:27:46 +0000

    introduction added

--- lib/our_experience_web/pages/welcome_live.ex
+++ lib/our_experience_web/pages/welcome_live.ex
@@ -1,13 +1,10 @@
 defmodule OurExperienceWeb.Pages.WelcomeLive do
   use OurExperienceWeb, :live_view
+  alias OurExperienceWeb.Pages.Public.Intro.DocsAsFuncs
 
   on_mount {OurExperienceWeb.Auth.AuthForLive, :matchThisInner}
 
   def mount(_params, session, socket) do
-    # dbg ["mount welcome",socket]
-    # {:ok, socket}
-    user = Map.get(session, "current_user")
-    dbg(["user in private live mount: ", user])
     {:ok, assign(socket, :current_user, Map.get(session, "current_user"))}
   end
 
@@ -16,9 +13,9 @@ defmodule OurExperienceWeb.Pages.WelcomeLive do
     <div class="container text-center">
       <h2 class="">Welcome to <strong>Our Experience</strong> project</h2>
       <p class="">(Created by Miroslav Makarov)</p>
-      <br />
       <p>...work in progress...</p>
     </div>
+      <DocsAsFuncs.introduction/>
     """
   end
 end
