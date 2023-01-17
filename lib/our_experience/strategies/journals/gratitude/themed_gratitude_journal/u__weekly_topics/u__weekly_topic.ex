76ed60d08474dff0c68caae72c20c7705b4eb377
MIROslavMakarov <miroslav.makarov@gmail.com>
2023-01-17 08:06:56 +0000

    user_id added to u_weekly_topics

--- lib/our_experience/strategies/journals/gratitude/themed_gratitude_journal/u__weekly_topics/u__weekly_topic.ex
+++ lib/our_experience/strategies/journals/gratitude/themed_gratitude_journal/u__weekly_topics/u__weekly_topic.ex
@@ -1,4 +1,5 @@
 defmodule OurExperience.Strategies.Journals.Gratitude.ThemedGratitudeJournal.U_WeeklyTopics.U_WeeklyTopic do
+  alias OurExperience.Users.User
   alias OurExperience.U_Strategies.U_Strategy
   alias OurExperience.Strategies.Journals.Gratitude.ThemedGratitudeJournal.WeeklyTopics.WeeklyTopic
   use Ecto.Schema
@@ -8,6 +9,7 @@ defmodule OurExperience.Strategies.Journals.Gratitude.ThemedGratitudeJournal.U_W
     field :active, :boolean, default: false
     field :position, :integer
 
+    belongs_to :user, User
     belongs_to :u_strategy, U_Strategy
     belongs_to :weekly_topic, WeeklyTopic
 
---
+++ priv/repo/migrations/20230117080328_link_users_to_user_weekly_topics.exs
@@ -0,0 +1,9 @@
+defmodule OurExperience.Repo.Migrations.LinkUsersToUserWeeklyTopics do
+  use Ecto.Migration
+
+  def change do
+    alter table (:u_weekly_topics) do
+       add :user_id, references(:users, on_delete: :nothing)
+    end
+  end
+end
