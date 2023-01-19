d443c4878c9dc4e8382491e2b709d40f33f4a3db
MIROslavMakarov <miroslav.makarov@gmail.com>
2023-01-19 18:48:06 +0000

    seeds added

---
+++ lib/our_experience/seeds/strategies_seeds.ex
@@ -0,0 +1,11 @@
+defmodule OurExperience.Seeds.StrategiesSeeds do
+  alias OurExperience.Strategies.Strategy
+  alias OurExperience.Repo
+
+  def createStrategy() do
+    Repo.insert!(%Strategy{
+      name: "Themed Gratitude Journal_" <> (DateTime.utc_now() |> DateTime.to_string())
+    })
+  end
+
+end
